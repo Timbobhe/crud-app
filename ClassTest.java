5656b380f12a01d169869797997f027420ec3f4a
Timbobhe <diallo.mamadou099@gmail.com>
2015-12-04 12:39:29 +0100

    on commit

---
+++ ClassTest.java
@@ -0,0 +1,26 @@
+package fr.esiag.esidis;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+public class ClassTest {
+	
+	@Test
+	public void showUpdate() {
+		//given 
+		
+		
+		//when 
+		
+		
+		//then
+		;
+	}
+	
+	
+}
